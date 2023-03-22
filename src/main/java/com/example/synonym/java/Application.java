0c6116bc69b690e9cc5c1e31a170ec263bcf00bf
mirza <mirza@olivebh.com>
2023-03-22 09:45:47 +0100

    synonyms - transitive rule implemented

--- src/main/java/com/example/synonym/java/Application.java
+++ src/main/java/com/example/synonym/java/Application.java
@@ -21,10 +21,24 @@ public class Application implements CommandLineRunner {
   @Override
   public void run(final String... args) {
     synonymService.addSynonym("important", "large");
+    synonymService.addSynonym("big1", "large");
     synonymService.addSynonym("important", "big");
     synonymService.addSynonym("important", "critical");
     synonymService.addSynonym("important", "BIG");
+  
+    synonymService.addSynonym("large", "string");
+    synonymService.addSynonym("string", "prvi");
+    synonymService.addSynonym("nevezano", "nevezano1");
+    synonymService.addSynonym("nevezano", "nevezano");
+    synonymService.addSynonym("big1", "Large");
+    synonymService.addSynonym("large", "laptop");
+    synonymService.addSynonym("nevezano", "big");
+    synonymService.addSynonym("tv", "tv1");
+    synonymService.addSynonym("tv2", "tv1");
+    synonymService.addSynonym("tv3", "tv2");
+    synonymService.addSynonym("tv2", "tv4");
+    synonymService.addSynonym("tv2", "laptop");
     
-    
+  
   }
 }
--- src/main/java/com/example/synonym/java/service/SynonymService.java
+++ src/main/java/com/example/synonym/java/service/SynonymService.java
@@ -20,35 +20,66 @@ public class SynonymService {
   private boolean synonymMapContainsWord(final String word1, final String word2) {
     Set<String> synonyms = new HashSet<>();
     if (!synonymTable.containsKey(word1)) {
-      synonyms.add(word2);
-      synonymTable.put(word1, synonyms);
-      //adding the values and returning the truth
       return false;
     }
     return true;
   }
   
+  private void addingValuesIntoMap(final String word1, final String word2) {
+    Set<String> emptySet = new HashSet<>();
+    //if map contans key with word1 value, then find it and add word2 synonym
+    
+    if (synonymMapContainsWord(word1.toLowerCase(), word2.toLowerCase())) {
+      synonymTable.get(word1.toLowerCase()).add(word2.toLowerCase());
+    } else {
+      emptySet.add(word2.toLowerCase());
+      synonymTable.put(word1.toLowerCase(), emptySet);
+    }
+    
+  }
   
   //add synonyms in both ways
   public String addSynonym(final String word1, final String word2) {
     
-    //if map contans key with word1 value, then find it and add word2 synonym
-    if (synonymMapContainsWord(word1.toLowerCase(), word2.toLowerCase())) {
-      synonymTable.get(word1.toLowerCase()).add(word2.toLowerCase());
+    if (word1.toLowerCase().equals(word2.toLowerCase())) {
+      return "Word and it's synonym are the same and can't be added";
+    }
+    addingValuesIntoMap(word1, word2);
+    addingValuesIntoMap(word2, word1);
+    transitiveRule(word1.toLowerCase(), word2.toLowerCase());
+    return "Synonym has been added succesufully";
+  }
+  
+  private void transitiveRule(final String word1, final String word2) {
+    addingValuesIntoMap(word1, word2);
+    addingValuesIntoMap(word2, word1);
+    for (String word : synonymTable.get(word1)) {
+      if (!word.equals(word2)) {
+        synonymTable.get(word).add(word2);
+        synonymTable.get(word2).add(word);
+      }
+      if (!synonymTable.get(word2).isEmpty()) {
+        for (String word01 : synonymTable.get(word2)) {
+          if (!word.equals(word01)) {
+            synonymTable.get(word).add(word01);
+            synonymTable.get(word01).add(word);
+          }
+        }
+      }
+    }
+    for (String word : synonymTable.get(word2)) {
+      if (!word.equals(word1)) {
+        synonymTable.get(word).add(word1);
+        synonymTable.get(word1).add(word);
+      }
     }
     
-    //the same if as previos, just the other way around
-    if (synonymMapContainsWord(word2.toLowerCase(), word1.toLowerCase())) {
-      synonymTable.get(word2.toLowerCase()).add(word1.toLowerCase());
-    }
-    return "Synonym has been added succesufully";
   }
   
   public Set<String> findSynonymsForWord(final String word) {
     //if word can't be found, we will return the empty set of values
     if (!synonymTable.containsKey(word.toLowerCase())) {
       Set<String> synonyms = new HashSet<>();
-      //synonymTable.put(word, synonyms);
       return synonyms;
     }
     //else we will return list of synonyms for requested word
